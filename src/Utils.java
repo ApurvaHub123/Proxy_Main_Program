a6e9a5e45b3650843bf1a8ee544c1fac8424d014
admin <apurva.g@toranainc.com>
2023-05-17 12:02:59 +0530

    added push msg

--- src/Utils.java
+++ src/Utils.java
@@ -615,6 +615,7 @@ public class Utils {
 	}
 
 	public boolean push(GitVO gitVo) {
+		System.out.println("Pushing.......");
 		Git git2 = null;
 		boolean result = false;
 		File file = new File(Constants.LOCAL_DIRECTORY_PATH);
